0abe151abf6b13dc6deb9aece5e9b90083810328
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-25 15:29:02 -0600

    multiline strings and whatnot

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -5,5 +5,32 @@ describe('template strings', function () {
     var string = `abc`;
 
     expect(string).to.be.a('string');
-  })
+  });
+
+  it('can evaulate variables', function() {
+    var bar = 'baz';
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can execute functions', function() {
+    var string = `The date is: ${new Date()}`
+    var date = new Date();
+
+    expect(string).to.equal('The date is: '+date);
+  });
+
+  it('can do math?', function () {
+    var string = `${1+1}`;
+
+    expect(string).to.equal('2');
+  });
+
+  it('can do multiline strings', function() {
+    var string = `hello
+ world`;
+
+    expect(string).to.equal('hello\n world');
+  });
 });
